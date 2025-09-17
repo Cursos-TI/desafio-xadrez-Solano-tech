d162322da6c8529c30d8017d17d532d93e8ecb35
Solano Andrade <202502014791@alunos.estacio.br>
2025-09-17 04:10:38 +0000

    Desafio novato em andamento

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,67 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Rainha: 8 casas para a esquerda
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Declaração de variáveis
+    int casaTorre = 5;
+    int casaBispo = 5;
+    int casaRainha = 8;
+    int i = 1;
+    int opcao;
+    
+    // Escolher a peça que será movimentada
+    printf("*** Movimentar peças de Xadrez ***\n");
+    printf("1. Bispo\n");
+    printf("2. Torre\n");
+    printf("3. Rainha\n");
+    printf("Selecione a peça a ser movimentada: ");
+    scanf("%d", &opcao);
+    
+    switch(opcao){
+        case 1:
+            // Implementação de Movimentação do Bispo
+            // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.  
+            printf("Peça escolhida: BISPO - Mover 5 casas diagonal superior direita\n");
+            while (i <= casaBispo)
+            {
+                printf("%d- Superior e Direita\n", i);
+                //printf("Direita\n");
+                //printf("%d\n", i);
+                i++;
+            }             
+        break;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+        case 2:
+            // Implementação de Movimentação da Torre
+            // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+            printf("Peça escolhida: TORRE - Mover 5 casas para direita\n");
+            do
+            {
+                printf("%d- Direita\n", i);
+                i++;
+            } while (i <= casaTorre);
+                       
+        break;
+        
+        case 3:
+            // Implementação de Movimentação da Rainha
+            // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+            // 
+        break;
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+        default:
+            printf("Opção inválida!\n");
+        break;
+    }
+
+
+
+
+
+
+
+    
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
